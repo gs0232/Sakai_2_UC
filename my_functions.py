b64705d2781be1c37fa0017976f12e187d888392
pm6953 <pm.schratt@mci4me.at>
2025-03-24 12:35:22 +0100

    Add files via upload
    von Pia Schratt

--- my_functions.py
+++ my_functions.py
@@ -1,6 +1,6 @@
 def estimate_max_hr(age_years : int , sex : str) -> int:
   """
-  See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4124545/ Add to Citavi project by PMC ID for different formulas
+  See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4124545/ Titel anhand dieser PMC-ID in Citavi-Projekt übernehmen for different formulas
   """
   if sex == "male":
     max_hr_bpm =  223 - 0.9 * age_years
@@ -27,3 +27,4 @@ def build_experiment(experiment_name, date, supervisor, subject) -> dict:
             "subject" :   subject
             }
     return experiment_dict
+    
